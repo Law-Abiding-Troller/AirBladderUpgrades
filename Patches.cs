3e9795b700aab46c887e89faf512c771fb8870db
Law Abiding Troller <colton.trash5.5@proton.me>
2025-03-31 13:28:54 -0500

    Update Patches.cs

--- Patches.cs
+++ Patches.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,18 @@ using AirBladderUpgrades;
 
 namespace AirBladderUpgrades
 {
-    [HarmonyPatch(typeof(AirBladder))]
+    [HarmonyPatch(typeof(AirBladder))] //patch the air bladder
     public class AirBladderPatches
     {
-        [HarmonyPatch(nameof(AirBladder.UpdateInflateState))]
+        [HarmonyPatch(nameof(AirBladder.UpdateInflateState))] //patch the updateinflatestate method, which checks the current capacity
         [HarmonyPrefix]
-        public static void UpdateInflateState_Prefix(AirBladder __instance)
+        public static void UpdateInflateState_Prefix(AirBladder __instance) //change the capcity before it checks calls the method
         {
-            if (__instance == null)
+            if (__instance == null) //check if the instance is null
             {
                 return;
             }
-            Plugin.defaults[Plugin.select] = __instance.oxygenCapacity;
-            __instance.oxygenCapacity += Plugin.currentcapacity;
+            __instance.oxygenCapacity += Plugin.currentcapacity; //add, not going to be that much
             Plugin.currentcapacity = 0;
         }
     }
