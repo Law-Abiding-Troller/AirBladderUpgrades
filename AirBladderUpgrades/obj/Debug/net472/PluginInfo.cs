f6e79bc2d170e5b02e23d9c53215f32470f06099
Law Abiding Troller <colton.trash5.5@proton.me>
2025-06-05 12:18:02 -0500

    Changes to Non-C# files by Rider.

---
+++ AirBladderUpgrades/obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace AirBladderUpgrades
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "AirBladderUpgrades";
+        public const string PLUGIN_NAME = "AirBladderUpgrades";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
